545a147773895a766d046be271345ddb30d86d43
Silent1852 <marker.1337@yandex.ru>
2022-11-12 19:31:47 +0700

    ver1

--- Lab5/GSON/app/src/main/java/com/example/gson/MainActivity.kt
+++ Lab5/GSON/app/src/main/java/com/example/gson/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.gson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
+import com.google.gson.GsonBuilder
+import okhttp3.*
 import timber.log.Timber
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,40 +13,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
 
-        val dataURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1"
-        val wrapped : Wrapper =
-            Gson().fromJson(dataURL,Wrapper::class.java)
-
-        val firstPage: PhotoPage =
-            Gson().fromJson(wrapped.photo, PhotoPage::class.java)
-
-        val photos =
-            Gson().fromJson(firstPage.photo, Array<Photo>::class.java).toList()
-
-        println(photos)
+        Thread{
+            getRequest()
+        }.start()
     }
 }
 
 data class Photo(
-    val id: Long,
+    val id: String,
     val owner: String,
     val secret: String,
-    val server: Int,
+    val server: String,
     val farm:  Int,
     val title: String,
-    val isPublic: Int,
-    val isFriend: Int,
-    val isFamily: Int
+    val ispublic: Int,
+    val isfriend: Int,
+    val isfamily: Int
 )
 
 data class PhotoPage(
     val page: Int,
     val pages: Int,
     val perPage: Int,
-    val photo: JsonArray
+    val photo: List<Photo> = listOf()
 )
 
 data class Wrapper(
-    val photo: JsonObject,
+    val photos: PhotoPage? = null,
     val stat: String = "ok"
 )
+
+fun getRequest(){
+    val client = OkHttpClient()
+    val request = Request.Builder()
+        .url("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
+        .build()
+
+    client.newCall(request).execute().use { response ->
+        if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+        val json = response.body?.string()
+        val gsBuilder = GsonBuilder()
+        val gs = gsBuilder.create()
+        val wrapper: Wrapper = gs.fromJson(json, Wrapper::class.java)
+
+        val sizeOfJson: Int? = wrapper.photos?.photo?.size
+
+        for(i in 0 until sizeOfJson!!)
+            if (i % 4 == 0) Timber.d(gs.toJson(wrapper.photos?.photo?.get(i)))
+    }
+}
